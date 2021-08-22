471eadac784dae5190f98468337a406d80cc5645
Dapiya <79071461+Dapiya@users.noreply.github.com>
2021-08-22 14:04:53 +0800

    Update rpdgrib.py

--- HY_Plotter/rpdgrib/rpdgrib.py
+++ HY_Plotter/rpdgrib/rpdgrib.py
@@ -6,26 +6,33 @@ import numpy as np
 class Rpdgrib(object):
 
     def extract(fname, band_index):
-        if ".h5" in fname:
+        HDF_DATA = True
+        
+        try:
             init = h5py.File(fname, "r")
+        except:
+            init = netCDF4.Dataset(fname)
+            HDF_DATA = False
+        
+        if HDF_DATA:
             # get values
             fns = fname.split("_", -1)
             lons, lats = init["wvc_lon"][:], init["wvc_lat"][:]
             data_spd, data_dir = init["wind_speed"][:], init["wind_dir"][:]
             data_spd, data_dir = data_spd[:,:,band_index], data_dir[:,:,band_index]
-            data_time = fns[6]
+            data_time = str(init["wvc_row_time"][:][init["wvc_row_time"][:] != b''][-1]).replace("b","").replace("'","").strip()
+            sate_name = str(init.attrs["Platform_ShortName"][-1]).replace("b","").replace("'","").strip()
             # process values
             lons, lats = np.ma.array(lons, mask=lons == 1.7e+38, fill_value=-32768), np.ma.array(lats, mask=lats == 1.7e+38, fill_value=-32768)
             data_spd, data_dir = np.ma.array(data_spd, mask=data_spd == -32767, fill_value=-32768), np.ma.array(data_dir, mask=data_dir == -32767, fill_value=-32768)
             data_spd, data_dir = data_spd / 100 / 0.514, data_dir / 10
-            data_sat = str(init.attrs["Platform_ShortName"][:][init.attrs["Platform_ShortName"][:] != b''][-1]).replace("b","").replace("'","").strip()
-        if ".nc" in fname:
-            init = netCDF4.Dataset(fname)
+        else:
             # get values
             lons, lats = init.variables["wvc_lon"][:], init.variables["wvc_lat"][:]
             data_spd, data_dir = init.variables["wind_speed"][:], init.variables["wind_dir"][:]
             data_spd, data_dir = data_spd[:,:,band_index], data_dir[:,:,band_index]
             data_time = init.time_coverage_end
+            sate_name = init.platform
             # process values
             data_spd = data_spd / 0.514
-        return lats, lons, data_spd, data_dir, data_time, data_sat
+        return lats, lons, data_spd, data_dir, data_time, sate_name
