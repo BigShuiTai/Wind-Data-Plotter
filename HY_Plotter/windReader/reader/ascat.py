61849ccb108c68d65eb5fe559ec5cc11ca5ed5bb
BigShuiTai <79071461+BigShuiTai@users.noreply.github.com>
2022-09-15 13:15:28 +0800

    fixed a bug on wind speed calculation

--- HY_Plotter/windReader/reader/ascat.py
+++ HY_Plotter/windReader/reader/ascat.py
@@ -28,7 +28,7 @@ class ASCAT(object):
             res = init.pixel_size_on_horizontal.replace(".0", "").replace(" ","")
             # process values
             lons.fill_value = lats.fill_value = data_spd.fill_value = data_dir.fill_value = -32768
-            data_spd = data_spd / 0.514
+            data_spd = data_spd / 0.514 / 0.93
             lons[lons < 0] += 360
             latmin, latmax, lonmin, lonmax = georange
             lon_mean = (lonmin + lonmax) / 2
